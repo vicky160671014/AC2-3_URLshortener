b6521f53b1bd72834537a1807d9d4c3b9f2cf0b7
vicky160671014 <vicky160671014@gmail.com>
2023-11-16 22:36:38 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-//database
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error',()=>{
-  console.log('mongodb error!')
-})
-db.once('open',()=>{
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs',exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,17 +1,7 @@
-const mongoose = require('mongoose')
 const URL = require('../url')
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   URL.create([
     { longURL: 'https://www.google.com', shortURL: '12Abc' },
     { longURL: 'https://www.npmjs.com', shortURL: '45jof' },
